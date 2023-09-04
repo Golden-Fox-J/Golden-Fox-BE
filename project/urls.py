61538be7f72fc5d6ababe96cf7802a8687ac5749
almothana-almasri <mothana.almasri.99@gmail.com>
2023-09-04 08:04:53 +0000

    fixed login reserve

--- project/urls.py
+++ project/urls.py
@@ -40,8 +40,7 @@ urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("about/", TemplateView.as_view(template_name="about.html"), name="about"),
     path("account/", include("account.urls")),
-    # path("accounts/", include("django.contrib.auth.urls")),
-      
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
 
 if settings.DEBUG:
